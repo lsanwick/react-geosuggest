ab0e427e638407551663bf2473e51f162ce4b488
Luke Sanwick <lsanwick@gmail.com>
2016-05-25 18:46:51 -0700

    Add `debounce` parameter to Geosuggest, defaulted to 250ms.

--- src/Geosuggest.jsx
+++ src/Geosuggest.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import classnames from 'classnames';
+import {debounce} from 'lodash';
 
 import defaults from './defaults';
 import propTypes from './prop-types';
@@ -32,6 +33,14 @@ class Geosuggest extends React.Component {
       suggests: [],
       timer: null
     };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onAfterInputChange = this.onAfterInputChange.bind(this);
+
+    if (props.debounce) {
+      this.onAfterInputChange =
+        debounce(this.onAfterInputChange, props.debounce);
+    }
   }
 
   /**
@@ -78,10 +87,12 @@ class Geosuggest extends React.Component {
    * @param {String} userInput The input value of the user
    */
   onInputChange(userInput) {
-    this.setState({userInput}, () => {
-      this.showSuggests();
-      this.props.onChange(userInput);
-    });
+    this.setState({userInput}, this.onAfterInputChange);
+  }
+
+  onAfterInputChange() {
+    this.showSuggests();
+    this.props.onChange(this.state.userInput);
   }
 
   /**
@@ -317,7 +328,7 @@ class Geosuggest extends React.Component {
                  ref='input'
                  autoComplete='off'
                  value={this.state.userInput}
-                 onChange={this.onInputChange.bind(this)}
+                 onChange={this.onInputChange}
                  onFocus={this.onInputFocus.bind(this)}
                  onBlur={this.onInputBlur.bind(this)}
                  onNext={() => this.activateSuggest('next')}
