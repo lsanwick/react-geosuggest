1b3d7a16a01d0a3bb289c1a26352884594584889
Luke Sanwick <lsanwick@gmail.com>
2016-06-15 11:33:30 -0700

    Commit build artifacts to enable github install

---
+++ module/defaults.js
@@ -0,0 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * Default values
+ */
+exports.default = {
+  fixtures: [],
+  initialValue: '',
+  placeholder: 'Search places',
+  disabled: false,
+  className: '',
+  inputClassName: '',
+  location: null,
+  radius: null,
+  bounds: null,
+  country: null,
+  types: null,
+  queryDelay: 250,
+  googleMaps: null,
+  onActivateSuggest: function onActivateSuggest() {},
+  onSuggestSelect: function onSuggestSelect() {},
+  onFocus: function onFocus() {},
+  onBlur: function onBlur() {},
+  onChange: function onChange() {},
+  skipSuggest: function skipSuggest() {},
+  getSuggestLabel: function getSuggestLabel(suggest) {
+    return suggest.description;
+  },
+  autoActivateFirstSuggest: false,
+  style: {
+    'input': {},
+    'suggests': {},
+    'suggestItem': {}
+  },
+  ignoreTab: false
+};
